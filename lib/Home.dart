6eb60ec6ea8c3c80ea69c6237cc0b453800f608d
Marcelo Bandeira <marceloband.93@gmail.com>
2021-01-04 22:42:28 -0300

    edit List<Widget>

--- lib/Home.dart
+++ lib/Home.dart
@@ -16,7 +16,7 @@ class _HomeState extends State<Home> {
 
   Widget build(BuildContext context) {
 
-    List<Widget> telas = [git
+    List<Widget> telas = [
       Inicio(),
       EmAlta(),
       Incricoes(),
